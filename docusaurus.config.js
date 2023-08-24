84b7e3d1bc5e11d015f23d80a2377c0da1e07fb0
JYC0413 <juyichen0413@foxmail.com>
2023-08-24 18:29:51 +0800

    fix
    Signed-off-by: JYC0413 <juyichen0413@foxmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/flows-network/docs/tree/main',
+            'https://github.com/flows-network/docs/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
